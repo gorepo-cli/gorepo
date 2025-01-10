12751ba9da4afdc39efe960f85f93b3252104238
Ezra Fayet <evrard.fayet.feret@gmail.com>
2025-01-10 23:48:39 +0200

    formated output of list

--- internal/commands/list/list.go
+++ internal/commands/list/list.go
@@ -1,9 +1,12 @@
 package list
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"gorepo-cli/internal/config"
 	"gorepo-cli/internal/flags"
+	"text/tabwriter"
 )
 
 func list(dependencies *config.Dependencies, cmdFlags *flags.CommandFlags, globalFlags *flags.GlobalFlags) error {
@@ -17,8 +20,16 @@ func list(dependencies *config.Dependencies, cmdFlags *flags.CommandFlags, globa
 	if len(modules) == 0 {
 		dependencies.Effects.Logger.InfoLn("no modules found")
 	} else {
+		var buf bytes.Buffer
+		w := tabwriter.NewWriter(&buf, 1, 1, 8, ' ', 0)
 		for _, module := range modules {
-			dependencies.Effects.Logger.DefaultLn(module.Name)
+			_, _ = fmt.Fprintln(w, fmt.Sprintf("%s\t%d\t%s\t%s\t%s\t", module.Name, module.Priority, module.Type, module.Template, module.Language))
+		}
+		_ = w.Flush()
+		for _, line := range bytes.Split(buf.Bytes(), []byte("\n")) {
+			if len(line) > 0 {
+				dependencies.Effects.Logger.DefaultLn(string(line))
+			}
 		}
 	}
 	return nil
